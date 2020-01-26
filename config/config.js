2aa8911b67ce842544a14b32ea5c322d21ccb891
Ed <ed@polarity.io>
2020-01-25 20:46:32 -0500

    Update option descriptions

--- config/config.js
+++ config/config.js
@@ -49,11 +49,11 @@ module.exports = {
     },
     {
       key: 'adminApiKey',
-      name: 'Redmine Administrator REST APIKey',
+      name: 'Redmine Administrator REST API Key',
       description:
         'A REST API Key for your Redmine administrator.  This key is used to retrieve user and status information when the integration first starts. The user information is used' +
         'to populate the Assignee field of the issue.  If not provided, the status and assignee fields will not be editable.  The Admin API Key is not used for performing searches,' +
-        ' editing issues, adding notes.',
+        ' editing issues, adding notes.  >> Please restart the integration after modifying this option.',
       default: '',
       type: 'password',
       userCanEdit: false,
@@ -61,7 +61,7 @@ module.exports = {
     },
     {
       key: 'apiKey',
-      name: 'Redmine REST APIKey',
+      name: 'Redmine User REST API Key',
       description:
         'The REST API Key used to authenticate to your Redmine instance. If left blank, no authentication will be used when communicating with the specified Redmine instance',
       default: '',
@@ -72,7 +72,7 @@ module.exports = {
     {
       key: 'project',
       name: 'Project to Search',
-      description: 'The name of a single project to search.  If left blank, all projects will be searched.',
+      description: 'The name of a single project to search.  If left blank, all projects will be searched.  Project name should be all lowercase with dashes in place of spaces.',
       default: '',
       type: 'text',
       userCanEdit: true,
